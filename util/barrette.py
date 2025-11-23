835af458aa6459c5e9633eb5877a4d359920279a
Hemanth23-He <goganaboinahemanth@gmail.com>
2025-11-23 07:52:01 +0530

    Update barrette.py

--- util/barrette.py
+++ util/barrette.py
@@ -1,21 +1,25 @@
-class BarrettReducer:
+class MontgomeryReducer:
     def __init__(self, modulus):
         self.modulus = modulus
-        self.k = modulus.bit_length() * 2
-        self.mu = (1 << self.k) // modulus
+        self.r = 1 << (modulus.bit_length() + 1)  # R > modulus
+        self.r_inv = pow(self.r, -1, modulus)
+        self.np = pow(-modulus, -1, self.r)
+    
+    def to_montgomery(self, x):
+        return (x * self.r) % self.modulus
+    
+    def from_montgomery(self, x):
+        return (x * self.r_inv) % self.modulus
 
     def reduce(self, x):
-        q = ((x * self.mu) >> self.k)
-        r = x - q * self.modulus
-        if r >= self.modulus:
-            r -= self.modulus
-        if r < 0:
-            r += self.modulus
-        return r
-    
+        m = ((x & (self.r - 1)) * self.np) & (self.r - 1)
+        t = (x + m * self.modulus) >> self.r.bit_length() - 1
+        if t >= self.modulus:
+            t -= self.modulus
+        return t
+
     def inverse(self, x):
-        # Modular inverse using extended Euclidean algorithm
-        # Returns y such that (x * y) % modulus == 1
+        # Modular inverse as before
         a, b, u = x, self.modulus, 1
         while b != 0:
             t = a // b
